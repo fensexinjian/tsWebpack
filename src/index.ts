ca5507d3e2f222a81249e826ad292759dc2368f4
闯出新天地 <1973890330@qq.com>
2022-04-19 16:03:53 +0800

    webpack

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
 /*
  * @Author: your name
- * @Date: 2022-04-18 17:31:23
- * @LastEditTime: 2022-04-18 17:42:53
+ * @Date: 2022-04-19 10:00:09
+ * @LastEditTime: 2022-04-19 14:50:15
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \webpack\src\index.ts
+ * @FilePath: \tsWebpack\file2\src\index.ts
  */
-import time from './indes'
+let people = {
+  age: 32,
+  name: "asasd",
+}
+let sum = 123123
+
 function num(a: number, b: number): number {
   return a + b
 }
-console.log(num(123, 345));
-console.log(time);
 
+
+console.log(num(123, 345));
+
+//获取当前年月日星期几几点几分几秒并打印
+function getTimer() {
+  var date = new Date()
+  var week = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var datee = date.getDate()
+  var day = date.getDay()
+  var time = year + '年' + month + '月' + datee + '日 	' + week[day]
+  return time
+}
+document.createElement('h1').innerHTML = getTimer()
+console.log(getTimer(), sum, people);
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,79 @@
-const path = require("path");
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// webpack中所有的配置信息都应该写在module.exports中
+// 引入一个包
+const path = require("path")
+
+//引入自动生成html的包
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+//引入更新dist文件的插件
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+// webpack中的所有的配置信息都应该写入moudle.exports中
 module.exports = {
-  mode: 'development',
   // 指定入口文件
   entry: "./src/index.ts",
   // 指定打包文件所在目录
   output: {
-    // 指定打包文件目录
-    path: path.resolve(__dirname, 'dist'),
-    // 打包后文件的文件
-    filename: "bundle.js"
+    // 指定打包文件的目录
+    path: path.resolve(__dirname, "dist"),
+    // 指定打包后文件的文件
+    filename: "bundle.js",
+    //编译的时候不使用箭头函数
+    environment: {
+      arrowFunction: false
+    }
   },
-  // 指定webpack打包时要使用的模块
+
+  //webpack打包时要使用到module这个模块
   module: {
     // 指定要加载的规则
-    rules: [
-      {
-        // test指定的是规则生成的文件
-        test: /\.ts$/,
-        // 要使用的loader
-        use: "ts-loader",
-        // 要排除的文件
-        exclude: /node-modules/
-
-      }
-    ]
+    rules: [{
+      // test指定的是规则生效的文件
+      test: /\.ts$/,
+      // use是要使用的loader
+      //配置babel
+      use: [
+        {//指定加载器
+          loader: "babel-loader",
+          options: {
+            //设置预定义环境
+            presets: [
+              [
+                //指定环境的插件
+                "@babel/preset-env",
+                //配置信息
+                {
+                  //要兼容的浏览器
+                  targets: {
+                    "chrome": "88"
+                  },
+                  // 指定corejs的版本
+                  "corejs": "3",
+                  //表示按需加载
+                  "useBuiltIns": "usage"
+                }
+              ]
+            ]
+          }
+        },
+        'ts-loader'
+      ],
+      //设置不被打包上传的文件
+      exclude: ["/node_modules/"]
+    }]
   },
+
   // 配置webpack插件
   plugins: [
-    // new CleanWebpackPlugin(),
-    new HTMLWebpackPlugin({
-      // title: "自定义标题",
-      // 自定义htm模板
+    new HtmlWebpackPlugin({
+      // 自定义html模板地址
       template: "./src/index.html"
     }),
+    //每次打包上线之前都会自动清空dist内的文件如何重新生成最新的文件
+    new CleanWebpackPlugin()
   ],
-  //用来设置引入模块
+
   resolve: {
+    //解决在ts文件内部单独引入其他ts包时候报错的问题
     extensions: ['.ts', '.js']
   }
-
-};
+}
